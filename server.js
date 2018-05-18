48e37b77ee9f9ebff58f589c5a9a3e7bebba0a58
mateuszdziuba <mkdziuba@gmail.com>
2018-05-18 19:30:47 +0200

    adding client

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const DATA_FILE = path.join(__dirname, 'data.json');
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use('/', express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
